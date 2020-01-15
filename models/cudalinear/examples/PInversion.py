affac71aeb966b4f50012ac5fa2aa78da5c16eb8
Lijun Zhu <ljzhu@gps.caltech.edu>
2020-01-14 16:34:35 -0800

    make cudalinear example work

--- models/cudalinear/examples/PInversion.py
+++ models/cudalinear/examples/PInversion.py
@@ -9,40 +9,40 @@
 
 #! /usr/bin/env python3
 
-import numpy 
+import numpy
 import h5py
 
 def PseudoInverse(gf_file, data_file):
     """
-    Use pseudo inverse to solve linear inversion problem 
+    Use pseudo inverse to solve linear inversion problem
     """
-    
+
     #load green's function
     gf = numpy.loadtxt(gf_file)
     # load data
     d = numpy.loadtxt(data_file)
-        
+
     # pinv
     gfinv = numpy.linalg.pinv(gf)
-    
+
     # inversion
-    theta = numpy.dot(gfinv, d) 
+    theta = numpy.dot(gfinv, d)
 
     # print out the result
     print("The parameters given by the pseudoinverse matrix\n", theta)
     # if needed, uncomment the following line to save it as well
-    #numpy.savetxt("theta_by_pinverse.txt", theta) 
+    #numpy.savetxt("theta_by_pinverse.txt", theta)
 
 
     h5 = h5py.File("results/step_final.h5", "r")
-    thetab = numpy.array(h5['theta'])
+    thetab = numpy.array(h5['ParameterSets/pset'])
     theta_m = numpy.mean(thetab, axis=0)
     theta_std = numpy.std(thetab, axis=0)
     print("The parameters given by the Bayesian inversion (mean values)\n", theta_m)
     print("The difference between them are\n", theta-theta_m)
     print("which are expected to be smaller than the standard deviations from Bayesian simultion\n", theta_std)
     # all done
-    return theta    
+    return theta
 
 
 if __name__ == '__main__':
