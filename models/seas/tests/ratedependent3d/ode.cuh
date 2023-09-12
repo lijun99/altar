780224f395eb76a1c3d5a7895d621704b136940f
tobiscode <47008700+tobiscode@users.noreply.github.com>
2023-09-12 14:03:56 -0700

    fix RateDependentODE.dydt tensor loop

--- models/seas/tests/ratedependent3d/ode.cuh
+++ models/seas/tests/ratedependent3d/ode.cuh
@@ -44,38 +44,38 @@ struct __ALIGNED__ RateDependentODE {
     // ode function
     __device__ __forceinline__ void dydt(const int system_id, const int patch_id, const T t, const T* y, T* f)
     {
-        // bool debug = ((threadIdx.x == 0) && (patch_id == 0));
+        // bool debug = ((threadIdx.x == 0) && (patch_id == 0) && (t < 86400 * 150) && (t > 86400 * 100));
 
         // update slip in both directions
         f[patch_id] = v_0 * exp(y[patch_id + 2 * patches]);
         f[patch_id + patches] = v_0 * exp(y[patch_id + 3 * patches]);
 
         // if (debug == true) {
-        //     printf("v_0 = %g\n", v_0);
-        //     printf("zeta_old = [%g, %g]\n", y[patch_id + 2 * patches], y[patch_id + 3 * patches]);
-        //     printf("ds/dt = v = [%g, %g]\n", f[patch_id], f[patch_id + patches]);
+        //     printf("v_0 = %.14g\n", v_0);
+        //     printf("zeta_old[%i] = [%.14g, %.14g]\n", patch_id, y[patch_id + 2 * patches], y[patch_id + 3 * patches]);
+        //     printf("ds/dt[%i] = v = [%.14g, %.14g]\n", patch_id, f[patch_id], f[patch_id + patches]);
         // }
 
         // update velocity in both directions
         f[patch_id + 2 * patches] = -K_ext[patch_id];
         f[patch_id + 3 * patches] = -K_ext[patch_id + patches];
-        auto temp0 = f[patch_id] - v_p[patch_id];
-        auto temp1 = f[patch_id + patches] - v_p[patch_id + patches];
 
         // if (debug == true) {
-        //     printf("-K_ext = [%g, %g]\n", -K_ext[patch_id], -K_ext[patch_id + patches]);
-        //     printf("v-v_p = [%g, %g]\n", temp0, temp1);
+        //     printf("-K_ext[%i] = [%.14g, %.14g]\n", patch_id, -K_ext[patch_id], -K_ext[patch_id + patches]);
         // }
 
         for (auto j = 0; j < patches; j++) {
-            f[patch_id + 2 * patches] += K_int[i_Kii(patch_id, 0, j, 0)] * temp0 + K_int[i_Kii(patch_id, 0, j, 1)] * temp1;
-            f[patch_id + 3 * patches] += K_int[i_Kii(patch_id, 1, j, 0)] * temp0 + K_int[i_Kii(patch_id, 1, j, 1)] * temp1;
+            auto delv0 = f[j] - v_p[j];
+            auto delv1 = f[j + patches] - v_p[j + patches];
+            f[patch_id + 2 * patches] += K_int[i_Kii(patch_id, 0, j, 0)] * delv0 + K_int[i_Kii(patch_id, 0, j, 1)] * delv1;
+            f[patch_id + 3 * patches] += K_int[i_Kii(patch_id, 1, j, 0)] * delv0 + K_int[i_Kii(patch_id, 1, j, 1)] * delv1;
 
             // if (debug == true) {
-            //     printf("    K_int[%i, :, %i, :] @ (v-v_p) = [%g, %g]\n",
+            //     printf("    (v-v_p)[%i] = [%.14g, %.14g]\n", j, delv0, delv1);
+            //     printf("    K_int[%i, :, %i, :] @ (v-v_p) = [%.14g, %.14g]\n",
             //            patch_id, j,
-            //            K_int[i_Kii(patch_id, 0, j, 0)] * temp0 + K_int[i_Kii(patch_id, 0, j, 1)] * temp1,
-            //            K_int[i_Kii(patch_id, 1, j, 0)] * temp0 + K_int[i_Kii(patch_id, 1, j, 1)] * temp1);
+            //            K_int[i_Kii(patch_id, 0, j, 0)] * delv0 + K_int[i_Kii(patch_id, 0, j, 1)] * delv1,
+            //            K_int[i_Kii(patch_id, 1, j, 0)] * delv0 + K_int[i_Kii(patch_id, 1, j, 1)] * delv1);
             // }
         }
 
@@ -84,9 +84,10 @@ struct __ALIGNED__ RateDependentODE {
         f[patch_id + 3 * patches] /= mu_over_2vs * f[patch_id + patches] + alpha_h[patch_id];
 
         // if (debug == true) {
-        //     printf("denom = [%g, %g]\n", mu_over_2vs * f[patch_id] + alpha_h[patch_id], mu_over_2vs * f[patch_id + patches] + alpha_h[patch_id]);
-        //     printf("ODE evaluation: t, v0, v1, dvdt0, dvdt1: %g %g %g %g %g\n",
-        //            t, f[patch_id], f[patch_id + patches], f[patch_id + 2 * patches], f[patch_id + 3 * patches]);
+        //     printf("denom[%i] = [%.14g, %.14g]\n", patch_id, mu_over_2vs * f[patch_id] + alpha_h[patch_id], mu_over_2vs * f[patch_id + patches] + alpha_h[patch_id]);
+        //     printf("ODE evaluation: t, i, v0, v1, dvdt0, dvdt1: %g %i %.14g %.14g %.14g %.14g\n",
+        //            t, patch_id, f[patch_id], f[patch_id + patches], f[patch_id + 2 * patches], f[patch_id + 3 * patches]);
+        // }
 
         return;
     };
@@ -137,4 +138,3 @@ struct __ALIGNED__ RateDependentODE {
 };
 
 #endif //__RateDependentODE_cuh__
-// end of file
