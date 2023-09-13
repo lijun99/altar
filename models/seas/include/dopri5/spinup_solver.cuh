90dc456108a4d852cbea81014a49e934052c1c64
tobiscode <47008700+tobiscode@users.noreply.github.com>
2023-09-13 13:47:18 -0700

    cherry-pick updates from seas-events branch
    ff3f032b7838e5f317b2c40ba6ec82ecea8b8bc3
    8d21e5a1c76234d7ea4a82b00316d4d656a0b155
    d109bda432ed1320c27f1875c0948eb996f9211f

--- models/seas/include/dopri5/spinup_solver.cuh
+++ models/seas/include/dopri5/spinup_solver.cuh
@@ -109,11 +109,16 @@ __global__ void solve_ivp_cycles_kernel(const int system_offset,
             outputter);
 
         // check for convergence
-        converged = spinup_controller.check_convergence(cta, stepper.yn);
+        converged = spinup_controller.check_convergence2(cta, stepper.yn, index_start, index_end);
         icycle++;
         cta.sync();
     }
     cta.sync();
+
+    // if debugging cycles
+    // if(cta.thread_rank()==0)
+    //    printf("spinup cycles finished in %d steps, convergence is %b\n", icycle, converged);
+
     //converged, last run for dense_out
     if(dense_out)
     {
