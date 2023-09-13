cef9933b67291be1d8becb8cfa448ca2b0f2b469
tobiscode <47008700+tobiscode@users.noreply.github.com>
2023-09-13 13:38:20 -0700

    ratedependent example debug first ode cycle

--- models/seas/tests/ratedependent3d/example.cu
+++ models/seas/tests/ratedependent3d/example.cu
@@ -74,7 +74,8 @@ int main()
     using T = double;
     using EventType = SEASEvents<T>;
     using OdeType = RateDependentODE<T>;
-    using SolverType = cuda::ode::dopri5::SpinupSolver<T, OdeType, EventType>;
+    // using SolverType = cuda::ode::dopri5::SpinupSolver<T, OdeType, EventType>;
+    using SolverType = cuda::ode::dopri5::Solver<T, OdeType, EventType>;
 
     // look for runfiles.json in current folder
     std::string rootdir = "./";
@@ -102,10 +103,10 @@ int main()
     int num_ix_break = shape[0];
     std::vector<int> ix_break_joint(read_bin_file<int>(rootdir + "ix_break_joint.bin", num_ix_break));
     vector_stats("ix_break_joint", ix_break_joint);
-    // std::copy(ix_break_joint.begin(),
-    //           ix_break_joint.end(),
-    //           std::ostream_iterator<int>(std::cout, " "));
-    // std::cout << std::endl;
+    std::copy(ix_break_joint.begin(),
+              ix_break_joint.end(),
+              std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
 
     // ix_eq_joint (num_ix_eq, )
     assert_string("ix_eq_joint.dtype", info.at("ix_eq_joint").at("dtype"), "int32");
@@ -114,10 +115,10 @@ int main()
     auto num_ix_eq = shape[0];
     std::vector<int> ix_eq_joint(read_bin_file<int>(rootdir + "ix_eq_joint.bin", num_ix_eq));
     vector_stats("ix_eq_joint", ix_eq_joint);
-    // std::copy(ix_eq_joint.begin(),
-    //           ix_eq_joint.end(),
-    //           std::ostream_iterator<int>(std::cout, " "));
-    // std::cout << std::endl;
+    std::copy(ix_eq_joint.begin(),
+              ix_eq_joint.end(),
+              std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
 
     // K_inner_inner_onfault (num_inner_patches, 2, num_inner_patches, 2)
     assert_string("K_inner_inner_onfault.dtype", info.at("K_inner_inner_onfault").at("dtype"), "float64");
@@ -203,11 +204,13 @@ int main()
     const int units = 4;
     const int systems = 1;
     const int systems_batch = 1;
-    const int max_cycles = 10;
+    // const int max_cycles = 1;
 
     // variable sizes
     int patches = num_inner_patches;
     int system_size = patches * units;
+
+    // initialize ODE
     OdeType odefunc {patches, units, systems, alpha_h_vec.data(), mu_over_2vs, v_0, K_inner_inner_onfault.data(),
                      K_inner_asperities_v_plate.data(), v_plate_ddcs_proj_eff_inner.data()};
 
@@ -221,46 +224,90 @@ int main()
         y0[i + 3 * patches] = (T) log(v_init[i_Kia_v(i, 1)] / v_0);
     }
 
-    // copy evaluation times
-    // the dense teval is the last cycle of t_eval_joint_sec
-    // bounded by the last two indices in ix_break_joint
-    // need to find it and reset to zero at cycle start
-    int ix_break_start = ix_break_joint[num_ix_break - 2];
-    int neval = num_t_eval - ix_break_start;
+    // // print first patch initial value
+    // printf("y0[patch=0]: [%g, %g, %g, %g]\n",
+    //        y0[0], y0[patches], y0[2 * patches], y0[3 * patches]);
+
+    // extract evaluation times
+
+    // first cycle (without observation times)
+    int ix_break_start = 0;
+    int ix_break_stop = ix_break_joint[1];
+
+    // // last cycle (with observation times)
+    // // the dense teval is the last cycle of t_eval_joint_sec
+    // // bounded by the last two indices in ix_break_joint
+    // // need to find it and reset to zero at cycle start
+    // int ix_break_start = ix_break_joint[num_ix_break - 2];
+    // int ix_break_stop = ix_break_joint[num_ix_break - 1];
+
+    // initialize teval
+    int neval = ix_break_stop - ix_break_start + 1;
     T * teval;
     cudaMallocManaged(&teval, neval * sizeof(T));
     for (auto i=0; i<neval; i++)
         teval[i] = t_eval_joint_sec[ix_break_start + i] - t_eval_joint_sec[ix_break_start];
 
-    // initialize dense output array
-    T * yeval;
-    cudaMallocManaged(&yeval, systems * neval * system_size * sizeof(T));
+    // print info
+    printf("ix_break: %i -> %i\n", ix_break_start, ix_break_stop);
+    printf("t_eval_joint_sec: %g -> %g\n",
+           t_eval_joint_sec[ix_break_start], t_eval_joint_sec[ix_break_stop]);
+    printf("teval: size=%i, first=%g, last=%g\n",
+           neval, teval[0], teval[neval-1]);
 
     // set myevents
     // the event times are defined in ix_eq_joint and are indices between
     // ix_break_start and ix_break_end
     // find first event index
-    int ix_eq_start;
+
+    // for first cycle
+    int ix_eq_start = 0;
+    int nevents = -1;
     for (auto i=0; i<num_ix_eq; i++) {
-        if (ix_eq_joint[i] >= ix_break_start) {
-            ix_eq_start = i;
+        if (ix_eq_joint[i] >= ix_break_joint[1]) {
+            nevents = i + 2;
             break;
         }
     }
+    if (nevents == -1) throw std::runtime_error("!! nevents == -1");
+
+    // // for last cycle
+    // int ix_eq_start = -1;
+    // for (auto i=0; i<num_ix_eq; i++) {
+    //     if (ix_eq_joint[i] >= ix_break_start) {
+    //         ix_eq_start = i;
+    //         break;
+    //     }
+    // }
+    // int nevents = num_ix_eq - ix_eq_start + 2;
+    // if (ix_eq_start == -1) throw std::runtime_error("!! ix_eq_start == -1");
+
     // extract event times from t_eval_joint_sec
-    int nevents = num_ix_eq - ix_eq_start + 2; // current format of nevents has to include the start and end time
+    // current format of nevents has to include the start and end time
     std::vector<double> tevents(nevents);
     tevents[0] = teval[0];
     tevents[nevents-1] = teval[neval - 1];
     for (auto i=0; i<nevents-2; i++)
         tevents[i+1] = t_eval_joint_sec[ix_eq_joint[ix_eq_start + i]] - t_eval_joint_sec[ix_break_start];
 
+    // print info
+    printf("ix_eq_start=%i\nnevents=%i\ntevents=[ ", ix_eq_start, nevents);
+    std::copy(tevents.begin(), tevents.end(),
+              std::ostream_iterator<double>(std::cout, " "));
+    printf("]\n");
+
+    // initialize events
     EventType events {nevents, tevents.data(), delta_tau_bounded.data(), alpha_h_vec.data(), patches, units};
 
+    // solver settings
     bool dense_out = true;
     bool use_y0_for_all = true;
-    const T atol = 1e-6;
-    const T rtol = 1e-7;
+    const T atol = 1e-10;
+    const T rtol = 1e-11;
+
+    // initialize dense output array
+    T * yeval;
+    cudaMallocManaged(&yeval, systems * neval * system_size * sizeof(T));
 
     // construct the ode solver
     SolverType solver{odefunc, events, atol, rtol, systems_batch};
@@ -276,25 +323,47 @@ int main()
         solver.set_init_values(y0, use_y0_for_all, systems_to_process, system_offset);
         // call the solver
         printf("iterate systems %d %d %d ... \n", system_offset, systems_to_process, systems);
-        solver.solve_ivp_cycles(dense_out, systems_to_process, system_offset, max_cycles);
+        // solver.solve_ivp_cycles(dense_out, systems_to_process, system_offset, max_cycles);
+        solver.solve_ivp(dense_out, systems_to_process, system_offset);
 
         cudaDeviceSynchronize();
 
-        // check results
-        for(auto is = max(0, systems_to_process-4); is<systems_to_process; is++)
-        {
-            auto system = is + system_offset;
-            std::cout << "System " << system << " ... \n";
-            for(auto ieval = 0; ieval<neval; ieval++)
-            {
-                std::cout << "t=" << std::setw(8) << teval[ieval] << " ";
-                for(auto i=0; i<min(8, patches*units); i++)
-                    std::cout << std::setw(8) << yeval[(system*neval+ieval)*patches*units+i] << " ";
-                std::cout << "...\n";
-            }
-        }
+        // // check results
+        // for (auto is = max(0, systems_to_process-4); is<systems_to_process; is++)
+        // {
+        //     auto system = is + system_offset;
+        //     std::cout << "System " << system << " ... \n";
+        //     for(auto ieval = 0; ieval<neval; ieval++)
+        //     {
+        //         std::cout << "t=" << std::setw(8) << teval[ieval] << " ";
+        //         for(auto i=0; i<min(8, patches*units); i++)
+        //             std::cout << std::setw(8) << yeval[(system*neval+ieval)*patches*units+i] << " ";
+        //         std::cout << "...\n";
+        //     }
+        // }
+
+        // write yeval output file
+        std::string outpath = "yeval_" + std::to_string(system_offset) + ".bin";
+        std::ofstream outfile(outpath, std::ios::out | std::ios::binary);
+        if (!outfile) {throw std::runtime_error("Could not write file: '" + outpath + "'");}
+        outfile.write((char *) &yeval[system_offset * neval * system_size * sizeof(T)],
+                      neval * system_size * sizeof(T));
+        outfile.close();
     }
 
-    //all done
+    // write teval output file
+    std::string outpath = "teval.bin";
+    std::ofstream outfile(outpath, std::ios::out | std::ios::binary);
+    if (!outfile) {throw std::runtime_error("Could not write file: '" + outpath + "'");}
+    outfile.write((char *) &teval[0], neval * sizeof(T));
+    outfile.close();
+
+    // print output file info
+    printf("Output file shape = %i (systems=%i, neval=%i, system_size=%i)\n",
+           systems * neval * system_size, systems, neval, system_size);
+
+    // // print first patch initial value
+    // printf("yeval[patch=0]: [%g, %g, %g, %g]\n",
+    //        yeval[0], yeval[patches], yeval[2 * patches], yeval[3 * patches]);
+
 }
-// end of file
